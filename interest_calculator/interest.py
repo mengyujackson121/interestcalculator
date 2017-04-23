452ba38ab809a97963a75dd7bdf5f047a03ae66f
mengyujackson121 <mengyujackson121@gmail.com>
2017-04-23 15:58:09 -0400

    test file and some data

---
+++ interest_calculator/interest.py
@@ -0,0 +1,20 @@
+def main():
+    month = 0
+    year = 30
+    t_month = 12 * year
+    thirty_mortgage_rate = - 4.5 / 100
+    thirty_mortgage = 1300
+    fifteen_mortgage_rate = - 2.75 / 100
+    fifteen_mortgage = 1791.56
+    different_thirty_fifty = fifteen_mortgage - thirty_mortgage
+    other = 392.98 + 32.74
+    free_monty = 2000
+    s_and_p_rate = 10 / 100
+
+    fifteen_income = 0
+    thirty_income = 0
+
+    while month <= t_month:
+
+
+main()
